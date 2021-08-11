828aee3dcc39dcceefdbd84a67e73d6a5a4e0d7b
sinaata25 <sina.ata25@gmail.com>
2021-08-11 05:31:27 -0700

    splash+login+logup

--- app/src/main/java/ataie/sina/picmoney/Urls.java
+++ app/src/main/java/ataie/sina/picmoney/Urls.java
@@ -6,4 +6,5 @@ public class Urls {
     public static String url_logup_check="http://192.168.1.105/picomoney/check_logup.php";
     public static String logup="http://192.168.1.105/picomoney/logup.php";
     public static String get_infos="http://192.168.1.105/picomoney/getcurrent_user.php";
+    public static String get_splash_info="http://192.168.1.105/picomoney/get_info.php";
 }
